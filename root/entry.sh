bd0ddbbef88a1826b992de51b30aee51070b42d1
A. Karl Kornel <akkornel@stanford.edu>
2023-11-29 00:06:14 -0800

    Add entrypoint command to run cron (in foreground)

--- root/entry.sh
+++ root/entry.sh
@@ -8,6 +8,7 @@ function usage {
 	echo "usage: $0 COMMAND ARGS"
 	echo
 	echo "The following commands are available:"
+	echo "- cron: Run the cron daemon (will not exit)"
 	echo "- mysql: Run the MariaDB client"
 	echo "- restic: Run the Restic client"
 }
@@ -19,6 +20,9 @@ if [ "${1:-x}" = "x" ]; then
 fi
 
 case $1 in
+	cron)
+		exec crond -f
+		;;
 	mysql)
 		exec mariadb "${@:2}"
 		;;
